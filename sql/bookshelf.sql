ea5a37be48191db7b51aa0eb5d94fc0faa77a79a
Alberto Arduini <albertoa679@gmail.com>
2021-09-13 11:00:39 +0200

    Updated db creation query to contain categories

--- sql/bookshelf.sql
+++ sql/bookshelf.sql
@@ -1,16 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Set 13, 2021 alle 10:59
+-- Versione del server: 10.4.14-MariaDB
+-- Versione PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bookshelf`
+--
 CREATE DATABASE IF NOT EXISTS `bookshelf` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `bookshelf`;
 
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `books`
+--
+
+DROP TABLE IF EXISTS `books`;
 CREATE TABLE `books` (
   `id` int(32) NOT NULL,
   `title` varchar(64) NOT NULL,
   `author` varchar(64) NOT NULL,
-  `category` int(32) NOT NULL,
+  `category` int(32) UNSIGNED DEFAULT NULL,
   `state` varchar(64) NOT NULL,
   `price` char(8) NOT NULL,
   `owner` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `id` int(32) UNSIGNED NOT NULL,
+  `name` char(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `users`
+--
+
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `email` varchar(64) NOT NULL,
   `password` char(64) NOT NULL,
@@ -18,16 +66,58 @@ CREATE TABLE `users` (
   `last_name` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `books`
+--
 ALTER TABLE `books`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_owner` (`owner`);
+  ADD KEY `fk_owner` (`owner`),
+  ADD KEY `fk_category` (`category`);
 
+--
+-- Indici per le tabelle `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`email`);
 
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `books`
+--
 ALTER TABLE `books`
   MODIFY `id` int(32) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT per la tabella `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(32) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `books`
+--
 ALTER TABLE `books`
+  ADD CONSTRAINT `books_category_fk` FOREIGN KEY (`category`) REFERENCES `categories` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `users` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
