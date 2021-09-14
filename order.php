10d81e5fc35d1b7d47475bbf2340f881108ae0c9
abtinzero <abtinsaadat@gmail.com>
2021-09-14 22:02:10 +0200

    Added order and orders (todo)

---
+++ order.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once("./pages_commons.php");
+
+$template_args[PAGE_TITLE] = "Order";
+$template_args[PAGE_BODY] = "./templates/order.php";
+
+include_once("./templates/page_base.php");
+
+?>
---
+++ seller_orders.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once("./pages_commons.php");
+
+$template_args[PAGE_TITLE] = "Dashboard";
+$template_args[PAGE_BODY] = "./templates/seller/orders.php";
+
+include_once("./templates/seller/page_base.php");
+
+?>
