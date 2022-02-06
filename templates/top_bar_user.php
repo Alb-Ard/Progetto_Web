f662f29dd9a84e1965f27a9b1c50e96451df1b71
Alberto Arduini <albertoa679@gmail.com>
2022-02-06 23:55:08 +0100

    Added automatic scrolling to menu when opened on mobile devices

--- templates/top_bar_user.php
+++ templates/top_bar_user.php
@@ -1,3 +1,13 @@
+<script type="text/javascript">
+    let menuOpen = false;
+
+    function toggleUserMenu() {
+        $('#user-menu').slideToggle();
+        $('#user-menu-avatar-anchor').attr("href", menuOpen ? "#" : "#user-menu");
+        menuOpen = !menuOpen;
+    }
+</script>
+
 <?php
 
 $show_user_info = is_user_logged();
@@ -34,7 +44,7 @@ if ($show_user_info) { ?>
     <?php
 
     if (!isset($template_args[PAGE_HIDE_NAVBAR]) || !$template_args[PAGE_HIDE_NAVBAR]) { ?>
-        <a class="black-link" href="#" onclick="$('#user-menu').slideToggle();">
+        <a id="user-menu-avatar-anchor" class="black-link" href="#" onclick="toggleUserMenu();">
             <?php print_user_avatar(); ?>
         </a>
     <?php } else {
