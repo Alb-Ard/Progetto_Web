3db47be9eef95bdd2b8068979bdb8b413a89942c
abtinzero <abtinsaadat@gmail.com>
2022-02-07 22:36:01 +0100

    formatting

--- templates/card_form.php
+++ templates/card_form.php
@@ -8,7 +8,7 @@
     <label class="col-12 col-md-6 justify-content-center form-label px-2 mb-4" for="cvv">CVV:
         <input class="form-control mt-1" type="text" id="cvv" name="cvv" placeholder="Insert cvv" value="" required/>
     </label>
-    <label class="col-12 justify-content-center form-label px-2 mb-4" for="state">Date:
+    <label class="col-12 col-md-6 justify-content-center form-label px-2 mb-4" for="state">Date:
         <input class="form-control mt-1" type="month" id="date" name="date" placeholder="Insert date" required<?php echo $card->date; ?>/>
     </label>
     <input class="col-12 justify-content-center btn button-primary m-0" type="button" id="confirm-button" onclick="<?php echo $on_confirm_func; ?>" value="<?php echo $on_confirm_value; ?>"/>
