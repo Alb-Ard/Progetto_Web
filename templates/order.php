0f420d5494a38985d68ac6e68aa9565f98dbb576
Alberto Arduini <albertoa679@gmail.com>
2022-02-12 01:48:55 +0100

    Modified confirm order page for accessibility

--- templates/order.php
+++ templates/order.php
@@ -14,46 +14,64 @@
         });
     }
 </script>
-<?php $card = $db_conn->get_payment_methods()->get_card($_GET["card"]); ?>
+
+<aside>
+    <p id="error-internal" class="row col-12 alert alert-danger login-alert" role="alert">Something went wrong! Check the values and try again.</p>
+</aside>
 
 <section>
-    <header class="row col text-center">
+    <header class="text-center">
         <h2>Confirm Order</h2>
     </header>
-    <ul class="p-3 d-flex flex-wrap justify-content-center" id="book-list">
-    <li class="col-5 col-md-1 row position-relative category-list-book">
-        
-                    <header class="card-header">
-                        <h3 class="card-title">
-                        <p class="stretched-link black-link"><?php echo $card->type; ?></a>
-                        </h3>
-                        <img class="col-12" src="./imgs/credit-card.png" alt="<?php echo $card->number; ?> image">
-                    <p class="col-12"><?php echo $card->number ?></p>
-                    </header>
-                </li>
-                </ul>
-                <?php $books = $db_conn->get_carted_books()->get_carted_books(get_client_info()["email"]); 
-                foreach($books as $book) {?>
-                <ul class="p-3 d-flex flex-wrap justify-content-center" id="book-list">
-                    <li class="card shadow m-3">
+    <div class="row">
+        <section class="col-12 col-lg-10">
+            <header>
+                <h3 class="visually-hidden">Cart</h3>
+            </header>
+            <ul class="p-3 d-flex flex-wrap justify-content-center" id="book-list">
+                <?php 
+                
+                $books = $db_conn->get_carted_books()->get_carted_books(get_client_info()["email"]); 
+                $total = 0;
+
+                foreach($books as $book) { 
+                    $total += floatval($book->price);
+                    ?>
+                    <li class="card card-no-hover shadow m-3">
                         <header class="card-header">
-                            <h3 class="text-truncate card-title">
-                                <p class="black-link"><?php echo $book->title; ?></a>
-                            </h3>
+                            <h3 class="text-truncate card-title"><?php echo $book->title; ?></h3>
                         </header>
-                        <img class="book-cover m-2" src="<?php echo $book->image ?>" alt="<?php echo $book->title; ?> image">
-                        <p class="text-center"><strong><?php echo $book->price ?>€</strong></p><?php
-                } ?>
+                        <img class="book-cover m-2" src="<?php echo $book->image ?>" alt="<?php echo $book->title; ?> image"/>
+                        <p class="text-center"><strong><?php echo $book->price ?>€</strong></p>
                     </li>
+                    <?php } ?>
             </ul>
-    <section>
-        <p id="error-internal" class="row col-12 alert alert-danger login-alert" role="alert">Something went wrong! Check the values and try again.</p>
-        <?php
-        
-        $card = new payment_data(); 
-        $on_confirm_value = "Confirm";
-        $on_confirm_func = "onAddOrder();";?>
-        <form class="col-10 offset-1 col-md-6 offset-md-3 p-0 row text-center" method="post">
-        <input class="col-12 justify-content-center btn button-primary m-0" type="button" id="confirm-button" onclick="<?php echo $on_confirm_func; ?>" value="<?php echo $on_confirm_value; ?>"/>
-        </form>
-    </section>
+        </section>
+        <aside class="col-12 col-lg-2 border shadow bg-light p-3 h-100">
+            <?php 
+
+            $card = $db_conn->get_payment_methods()->get_card($_GET["card"]); 
+            $censored_card_number = "Ends with: " . substr($card->number, strlen($card->number) - (strlen($card->number) / 4));
+
+            ?>
+            <section class="border-bottom border-dark mb-3">
+                <header class="text-center py-1">
+                    <h3>Payment method</h3>
+                </header>
+                <div class="row">
+                    <div class="col-3 col-lg-12 col-xxl-3 my-auto">
+                        <img class="img-fluid" src="./imgs/credit-card.png" alt="card image">
+                    </div>
+                    <div class="col-9 col-lg-12 col-xxl-9">
+                        <p><?php echo $card->type; ?></p>
+                        <p><?php echo $censored_card_number ?></p>
+                    </div>
+                </div>
+            </section>
+            <form class="p-0 text-center" method="post" action="#">
+                <p>Total: <strong><?php echo $total; ?>€</strong></p>
+                <input class="w-100 btn button-primary m-0" type="submit" id="confirm-button" onclick="onAddOrder();" value="Confirm"/>
+            </form>
+        </aside>
+    </div>
+</section>
