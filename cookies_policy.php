1cc38dc56d20565c128bb79ba75a724b3c75ae4f
Alberto Arduini <albertoa679@gmail.com>
2022-02-12 10:25:15 +0100

    Fixed cookies page base template

--- cookies_policy.php
+++ cookies_policy.php
@@ -5,6 +5,6 @@ include_once("./pages_commons.php");
 $template_args[PAGE_TITLE] = "Cookies policy";
 $template_args[PAGE_BODY] = "./templates/cookies.php";
 
-include_once("./templates/seller/page_base.php");
+include_once("./templates/page_base.php");
 
 ?>
